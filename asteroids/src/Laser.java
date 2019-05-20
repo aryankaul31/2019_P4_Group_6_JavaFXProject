2c47787acd31ddb69b2ac9ba52371b0534450715
PranavEranki <pranav.eranki@gmail.com>
2019-05-20 09:16:40 -0700

    Adding comment

--- asteroids/src/Laser.java
+++ asteroids/src/Laser.java
@@ -4,6 +4,7 @@ public class Laser extends Actor {
 	int dx;
 	int dy;
 	double rotation;
+	double totalDistance;
 	
 	public Laser(int x, int y, double r) {
 		dx = x;
@@ -12,7 +13,13 @@ public class Laser extends Actor {
 	}
 	@Override
 	public void act() {
+		// move it in the direction of rotation
+		// by dx and dy
+		// add to total distance.
+		// if total distance > a threshhold, then delete this
+		
 		
 	}
+
 
 }
