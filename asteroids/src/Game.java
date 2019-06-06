bb00e7a873eff697278dcee3967a4a40c7d72676
Aryan Kaul <aryankaul31@gmail.com>
2019-06-06 08:06:12 -0700

    Help page

--- asteroids/src/Game.java
+++ asteroids/src/Game.java
@@ -1,9 +1,15 @@
 import java.util.Random;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.DialogEvent;
 import javafx.scene.control.Label;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.image.Image;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
@@ -54,10 +60,40 @@ public class Game extends Application{
 		ship.setX(250);
 		ship.setY(250);
 		world.add(ship);
-		hb.setSpacing(100);
+		hb.setSpacing(50);
 		hb.setStyle("-fx-background-color: #343d46;");
 		
-		
+		Button b = new Button();
+		b.setText("Help");
+		b.setOnAction(new EventHandler<ActionEvent>() {
+
+			public void handle(ActionEvent event) {
+				// TODO Auto-generated method stub
+				final Alert alert = new Alert(AlertType.INFORMATION);
+				alert.setTitle("Help");
+				alert.setContentText("Press the back arrow to go back, with deacceleration, in the opposite way spaceship is pointing\n" + 
+						"space to shoot lasers\n" + 
+						"c to clear your immunity\n" + 
+						"i to get immunity for a few minutes, for testing purposes\n" + 
+						"Asteroids spawn asteroid fragments when destroyed, and each fragment spawns 2 more\n" + 
+						"Enemy ships fly around fast, shoot at the player, avoid them\n" + 
+						"You have five lives, try and maximize your score\n" + 
+						"You get immunity by either giving yourself it, by completing a level, or by dying - you get immunity for like 2 seconds or so\n" + 
+						"Asteroids move randomly, so be careful\n" + 
+						"");
+				
+				alert.setOnHidden(new EventHandler<DialogEvent>() {
+					public void handle(DialogEvent evt) {
+						alert.close();
+					}
+				});
+				
+			    alert.show(); 
+
+			}
+			
+		});
+		hb.getChildren().add(b);
 		// TODO Auto-generated method stub
 		world.setOnKeyPressed(new EventHandler<KeyEvent>() {
 
@@ -87,5 +123,6 @@ public class Game extends Application{
 		
 		
 	}
+	
 
 }
