4220c5cff50d01fecc5aff4921b25ed42ba3e757
Aryan Kaul <aryankaul31@gmail.com>
2019-06-06 08:24:18 -0700

    Javadocs

--- asteroids/src/Explosions.java
+++ asteroids/src/Explosions.java
@@ -2,12 +2,22 @@ import java.util.ArrayList;
 
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-
+/**
+ * This class is the explosions class that extends actor. It is used to display an explosion when an asteroid 
+ * or the player or the enemy spaceship is killed.
+ *
+ */
 public class Explosions extends Actor{
-	ArrayList<Image> explosions = new ArrayList<Image>();
-	int state;
-	int steps;
 	
+	/** * ArrayList to keep track of the spaceships */
+	ArrayList<Image> explosions = new ArrayList<Image>();
+	/**Checking which state it is in to change the arrayList picture accordingly */
+	int state;
+	/**keeping track of the steps that are called every act */
+	int steps;
+	/**
+	 * Constructor for the explosions class, this puts the explosions into the arrayList and initializes the other 2 variables
+	 */
 	public Explosions() {
 		state = 0;
 		steps = 0;
@@ -16,6 +26,10 @@ public class Explosions extends Actor{
 		}
 		setState(0);
 	}
+	/**
+	 * Set method for the stat variable
+	 * @param n The new parameter for the  state
+	 */
 	
 	public void setState(int n) {
 		state = n;
@@ -27,7 +41,9 @@ public class Explosions extends Actor{
 			setFitWidth(30);
 		}
 	}
-
+/**
+ * Increases the state every 10 act methods to change the pictures accordingly.
+ */
 	@Override
 	public void act() {
 		steps ++;
--- asteroids/src/Score.java
+++ asteroids/src/Score.java
@@ -1,23 +1,33 @@
 import javafx.scene.text.Text;
 import javafx.scene.text.Font;
-
+/**
+ * Score class that extends text. This class is used  to keep track of the score for the player.
+ *
+ */
 public class Score extends Text{
+	/**Variable to keep track of the score */
 	private int score;
-		
+	/**Constructor with no parameters, this constructor sets a font and calls update display to show the text to the game */
 	public Score() {
 		score = 0;
 		this.setFont(Font.font (this.getFont().getSize() + 5));
 		updateDisplay();
 	}
-	
+	/**Updates the display for the player to see */
 	public void updateDisplay() {
 		this.setText("Score : " + Integer.toString(score));
 	}
-	
+	/**
+	 * Getter method for the score of the player
+	 * @return The current score of the player
+	 */
 	public int getScore() {
 		return score;
 	}
-	
+	/**
+	 * This method is used for other classes to update the score for the player
+	 * @param input the new score for the player
+	 */
 	public void setScore(int input) {
 		score = input;
 		updateDisplay();
