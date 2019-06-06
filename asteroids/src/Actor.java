dfc800f87f6cffa5ec11c95892f2f69d83ba479a
Pranav E <pranav.eranki@gmail.com>
2019-06-06 07:33:27 -0700

    Adding javadocs for actor.java

--- asteroids/src/Actor.java
+++ asteroids/src/Actor.java
@@ -5,22 +5,41 @@ import javafx.scene.image.ImageView;
 
 public abstract class Actor extends ImageView {
 
+	/**
+	 * abstract act method, meant to be overridden by all subclasses
+	 */
 	public abstract void act();
-
+	/**
+	 * Moves actor
+	 * @param dx - amount to move x
+	 * @param dy - amount to move y
+	 */
 	public void move(double dx, double dy) {
 		setX(getX() + dx);
 		setY(getY() + dy);
 	}
 
-	// get height
+	/**
+	 * Getter for width of actor
+	 * @return width
+	 */
 	public double getWidth() {
 		return getBoundsInParent().getWidth();
 	}
-
+	/**
+	 * Getter for height of actor
+	 * @return height
+	 */
 	public double getHeight() {
 		return getBoundsInParent().getHeight();
 	}
 
+	/**
+	 * Gets all intersecting objects of a certain class
+	 * @param <A> A class type
+	 * @param cls
+	 * @return A list of all intersecting objects of a certain class
+	 */
 	public <A extends Actor> java.util.List<A> getIntersectingObjects(java.lang.Class<A> cls) {
 		ArrayList<A> objects = new ArrayList<A>();
 
@@ -34,6 +53,13 @@ public abstract class Actor extends ImageView {
 
 		return objects;
 	}
+	
+	/**
+	 * Gets all intersecting objects
+	 * @param <A> Class type
+	 * @param cls
+	 * @return List of all objects in world of a particular type
+	 */
 
 	public <A extends Actor> A getOneIntersectingObject(java.lang.Class<A> cls) {
 		List<A> objects = getIntersectingObjects(cls);
@@ -45,6 +71,11 @@ public abstract class Actor extends ImageView {
 		}
 
 	}
+	
+	/**
+	 * Getter for world
+	 * @return World
+	 */
 
 	public World getWorld() {
 		return (World) getParent();
